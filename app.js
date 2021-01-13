23df577347ed43dc764a1abe2f92a61c6a34762a
Laura <laura.sfichi@yahoo.ro>
2021-01-13 23:27:53 +0200

    cat bg

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => {
+    console.log('hi')
     menuToggle.classList.toggle('active');
     showcase.classList.toggle('active');
 })
